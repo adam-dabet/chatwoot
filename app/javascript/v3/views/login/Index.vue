07d416d05e07e3ad31aafbba183ad786c89884ed
adam-dabet <adamdabet@gmail.com>
2025-05-26 14:16:27 -0700

    removed unused files and fixed logo sizing

--- app/javascript/v3/views/login/Index.vue
+++ app/javascript/v3/views/login/Index.vue
@@ -172,17 +172,19 @@ export default {
     <section class="max-w-5xl mx-auto">
       <img
         :src="globalConfig.logo"
-        alt="Logo"
+        :alt="globalConfig.installationName"
         class="block w-auto h-8 mx-auto dark:hidden"
       />
       <img
         v-if="globalConfig.logoDark"
         :src="globalConfig.logoDark"
-        alt="Logo"
+        :alt="globalConfig.installationName"
         class="hidden w-auto h-8 mx-auto dark:block"
       />
       <h2 class="mt-6 text-3xl font-medium text-center text-n-slate-12">
-        {{ $t('LOGIN.TITLE') }}
+        {{
+          useInstallationName($t('LOGIN.TITLE'), globalConfig.installationName)
+        }}
       </h2>
       <p v-if="showSignupLink" class="mt-3 text-sm text-center text-n-slate-11">
         {{ $t('COMMON.OR') }}
